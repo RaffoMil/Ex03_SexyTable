1379a887c09d20841a66f71b316b027a0cf73750
Antonio <ceraoloanto@gmail.com>
2022-12-05 15:59:30 +0100

    create sexyTable

---
+++ src/app/features/feature.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SexyTableComponent } from './sexyTable/sexy-table.component';
+
+
+
+@NgModule({
+  declarations: [
+    SexyTableComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class FeatureModule { }
